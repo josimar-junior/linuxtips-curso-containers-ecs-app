f91dad7f4f0017044687d58e285cf2d38a37cb0a
Josimar Junior <josimarjunior@Josimars-MacBook-Pro.local>
2026-01-16 08:00:17 -0300

    fix efs sg

--- terraform/efs.tf
+++ terraform/efs.tf
@@ -4,22 +4,25 @@ resource "aws_efs_file_system" "main" {
 }
 
 resource "aws_security_group" "efs" {
-  name   = format("%s-efs-sg", var.service_name)
-  vpc_id = data.aws_ssm_parameter.vpc_id
+  description = format("%s-efs-sg", var.service_name)
+  name        = format("%s-efs-sg", var.service_name)
+  vpc_id      = data.aws_ssm_parameter.vpc_id
 
-  ingress = {
-    from_port   = 2049
-    to_port     = 2049
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  ingress = [
+    {
+      from_port   = 2049
+      to_port     = 2049
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
 
-  egress = {
+  egress = [{
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
+  }]
 }
 
 resource "aws_efs_mount_target" "mount-1a" {
